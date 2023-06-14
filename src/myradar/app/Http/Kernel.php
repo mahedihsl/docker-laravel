29dc00c2d6dd52f7c210112ac00bda168f0bf617
mahedihsl <mahedihsl2022@gmail.com>
2023-06-14 12:11:32 +0600

    rms site list trottle per miniutes requesr change

--- src/myradar/app/Http/Kernel.php
+++ src/myradar/app/Http/Kernel.php
@@ -36,12 +36,15 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':100,1',
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':100,1',
+
         ],
     ];
 
